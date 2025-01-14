9250c95c99d289e55e4d1dddc2c2ba96ed15dc0b
Rafiulsam <rafiulsam69@gmail.com>
2025-01-15 02:05:14 +0600

    env file updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-const uri = process.env.URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sdmdnc2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
